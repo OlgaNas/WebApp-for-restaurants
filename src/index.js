5a2dc436dbc7b8e4fd4f175510fb1189157158e2
Olga Nastaja <olga.vlasova321@gmail.com>
2023-06-14 21:43:38 +0100

    add index.js index.html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
 import App from 'components/App/App';
 import { createRoot } from 'react-dom/client';
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
